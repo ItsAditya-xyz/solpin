228a34409df455780b963da885617359f0efb935
ItsAditya-xyz <chaudharyaditya0005@gmail.com>
2023-01-18 17:54:36 +0530

    profile page

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -4,33 +4,24 @@ import SplingContext from "../../Context/SplingContext/SplingContext";
 import { Loader } from "../../components/Loader";
 import { SocialProtocol } from "@spling/social-protocol";
 import { Link } from "react-router-dom";
-
+import Tippy from "@tippyjs/react";
+import toast, { Toaster } from "react-hot-toast";
 import { Keypair, PublicKey } from "@solana/web3.js";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+import styles from "../../styles/Home.module.css";
+import logo from "../../assets/logo.png";
 function ProfilePage(props) {
   const { publicKey } = useParams();
   const [publicKeyVal, setPublicKeyVal] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
   const SplingContextValue = useContext(SplingContext);
   const [socialProtocolValue, setSocialProtocolValue] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     if (!publicKey) return;
     setPublicKeyVal(publicKey);
   }, [publicKey]);
-
-  const convertBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  };
 
   useEffect(() => {
     if (!publicKeyVal) return;
@@ -43,9 +34,15 @@ function ProfilePage(props) {
       const userInfo = await socialProtocolValue.getUserByPublicKey(
         publicKeyObj
       );
-      console.log(userInfo);
-      setUserInfo(userInfo);
+      if (userInfo) {
+        setIsLoading(false);
+        setUserInfo(userInfo);
+      } else {
+        setIsLoading(false);
+        toast.error("Something went wrong. Please try again later.");
+      }
     }
+
     async function initSocialProtocol() {
       const protocolOptions = {
         useIndexer: true,
@@ -72,7 +69,41 @@ function ProfilePage(props) {
     }
   }, [publicKeyVal, socialProtocolValue]);
 
-  return <div>TEST</div>;
+  return (
+    <div>
+      <Toaster />
+      <div className={styles.AppHeader}>
+        <Link to="/">
+          <img src={logo} className="w-[45%] sm:w-[200px]" />
+        </Link>
+        <WalletMultiButton />
+      </div>
+      {isLoading && (
+        <div className="flex justify-center items-center h-screen">
+          <Loader />
+        </div>
+      )}
+      {!isLoading && userInfo && (
+        <div className="flex flex-col justify-center items-center mt-10">
+          <div className="flex  justify-center space-x-3">
+            <img
+              src={userInfo.avatar}
+              className="w-[120px] h-[120px] rounded-full"
+            />
+            <div className="flex flex-col ">
+              <p className="text-4xl font-bold mt-2">{userInfo.nickname}</p>
+              <p className="text-xl text-gray-800 mt-8">{userInfo.bio}</p>
+            </div>
+            <div className="mt-3">
+              <button className="px-4 py-2 bg-purple-500 hover:bg-purple-600 text-white rounded-full">
+                Folllow
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ProfilePage;
