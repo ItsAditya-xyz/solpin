3dd0f54b3123aab22d2513b7ea495022ff5a19ba
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2023-01-19 01:46:13 +0530

    new release

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import styles from "../styles/Home.module.css";
 import { Link } from "react-router-dom";
 export default function Navbar({ shouldShowWallet }) {
   return (
     <div className='bg-[#20252e] flex items-center justify-between px-2 py-3'>
       <Link to='/'>
-      <img src={logo} className='w-[160px]' />
+        <img src={logo} className='w-[160px]' alt="logo" />
       </Link>
       {shouldShowWallet && <WalletMultiButton />}
       {!shouldShowWallet && (
         <div className=''>
-          <Link className=' bg-[#512DA8] text-white mx-auto lg:mx-0 hover:bg-[#5D3DAA]  rounded my-2 md:my-6 py-4 px-8 shadow-lg w-70 hover:scale-105 text-sm'>
+          <Link
+            to='/'
+            className=' bg-[#512DA8] text-white mx-auto lg:mx-0 hover:bg-[#5D3DAA]  rounded my-2 md:my-6 py-4 px-8 shadow-lg w-70 hover:scale-105 text-sm'>
             Get Started
           </Link>
         </div>
