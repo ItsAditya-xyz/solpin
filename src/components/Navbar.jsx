e8e32b7ff8dfe498bd24ea585198168c356a0943
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2023-01-19 01:54:27 +0530

    added profile form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar({ shouldShowWallet }) {
       {!shouldShowWallet && (
         <div className=''>
           <Link
-            to='/'
+            to='/sign-up'
             className=' bg-[#512DA8] text-white mx-auto lg:mx-0 hover:bg-[#5D3DAA]  rounded my-2 md:my-6 py-4 px-8 shadow-lg w-70 hover:scale-105 text-sm'>
             Get Started
           </Link>
