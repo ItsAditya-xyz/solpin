5e12c8ba15900f58961ea746ad01a273545544f4
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2023-01-20 04:17:48 +0530

    updated sdk

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
+//if you think this shuld be in .env file, you don't know what you are doing LMAOO
 export const protocolOptions = {
   useIndexer: true,
   rpcUrl:
-    "https://rpc.helius.xyz/?api-key=b228ed66-73d6-4441-b7be-08ff999b346f",
+    "https://rpc.helius.xyz/?api-key=b228ed66-73d6-4441-b7be-08ff999b346f", 
 };
